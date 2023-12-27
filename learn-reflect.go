8b71b45c98249c8bc9819f48fcec757ee5390b77
Cameron <cameron-04@hotmail.com>
2023-12-26 21:43:20 -0600

    Value vs. Method for method pointers

--- learn-reflect.go
+++ learn-reflect.go
@@ -5,6 +5,17 @@ import (
 	"reflect"
 )
 
+type MyStruct struct {
+	A   int
+	Bee string
+	See float64
+}
+
+func (s *MyStruct) Foo(a int) bool {
+	fmt.Println(s.Bee)
+	return false
+}
+
 func a() {
 	var x float64 = 3.14
 	v := reflect.ValueOf(x)
@@ -70,11 +81,6 @@ func a() {
 	fmt.Println(ePtrVal.String()) // <*bool Value>
 
 	// Q. How do structs get represented when printed in various ways?
-	type MyStruct struct {
-		A   int
-		Bee string
-		See float64
-	}
 	f := MyStruct{A: 1, Bee: "bee", See: 3.14}
 	fVal := reflect.ValueOf(f)
 	fmt.Println(fVal)          // {1 bee 3.14}
@@ -118,4 +124,44 @@ func a() {
 	fmt.Println(jVal.Type())   // func(int) int
 	// A. Yes, this works differently than nil pointers to structs.
 
+	// Q. Difference between retrieving method on a Value vs a Type?
+	println("")
+	k := &MyStruct{A: 1, Bee: "bee", See: 3.14}
+	kVal := reflect.ValueOf(k)
+	kType := reflect.TypeOf(k)
+	valMethod := kVal.Method(0)
+	typeMethod := kType.Method(0)
+	fmt.Println(valMethod, "|", valMethod.Type(), "|", valMethod.Kind(), "|", valMethod.String())
+	fmt.Println(typeMethod, "|", typeMethod.Type, "|", typeMethod.Name, "|", typeMethod.Name)
+	fmt.Println(typeMethod.Func)        // 0x4bb1e0
+	fmt.Println(typeMethod.Func.Type()) // func(*main.MyStruct, int) bool
+	//fmt.Println(valMethod == typeMethod) // does not compile, Value is not comparable to Method
+	fmt.Println(valMethod == typeMethod.Func)               // false
+	fmt.Println(valMethod.Type() == typeMethod.Func.Type()) // false
+	valMethod.Call([]reflect.Value{reflect.ValueOf(12)})
+	typeMethod.Func.Call([]reflect.Value{reflect.ValueOf(k), reflect.ValueOf(13)}) // requires passing in the struct
+	// A. Main difference is that the Type method is an actual Method vs just a Value. A Method contains a pointer to
+	// the underlying method as a Value in its field Func. This Value contains the type info that it's a method on a
+	// struct, which the value version of the function pointer only knows the input and output typing. Additionally,
+	// calling the valMethod will invoke the method with the underlying struct instance in memory, but calling the
+	// typeMethod.Func will require a struct instance to be passed as the first argument (similar to python's "self")
+
+	// Q. Does each struct instance generate each method on the struct at instantiation?
+	l := &MyStruct{A: 1, Bee: "bee", See: 3.14}
+	m := &MyStruct{A: 1, Bee: "bee", See: 3.14}
+	lVal := reflect.ValueOf(l)
+	mVal := reflect.ValueOf(m)
+	fmt.Println(lVal.Method(0) == lVal.Method(0)) // the control, true
+	fmt.Println(lVal.Method(0) == mVal.Method(0)) // false
+	// A. Yes, each struct instance generates each method on the struct at instantiation.
+
+	// Q. Does the Method derived from a struct Type point to a shared function definition?
+	n := &MyStruct{A: 1, Bee: "bee", See: 3.14}
+	o := &MyStruct{A: 1, Bee: "bee", See: 3.14}
+	nType := reflect.TypeOf(n)
+	oType := reflect.TypeOf(o)
+	fmt.Println(nType.Method(0) == nType.Method(0))           // the control, false?
+	fmt.Println(nType.Method(0).Func == nType.Method(0).Func) // the control, false?
+	fmt.Println(nType.Method(0).Func == oType.Method(0).Func) // unsurprisingly, false
+	// A. No, even the control is false. So calling Method on a Type must generate a new function definition for each
 }
